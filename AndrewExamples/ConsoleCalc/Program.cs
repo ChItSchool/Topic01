f2569b9f28ab909a9bc9230be54487a1c4970d00
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-01-10 15:09:31 +0200

    Додано коментарі про послідовність виконання коду

--- AndrewExamples/ConsoleCalc/Program.cs
+++ AndrewExamples/ConsoleCalc/Program.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleCalc
 {
+    //Клас з якого починається виконання програми
     class Program
     {
+        //метод з якого починається виконання програми
         static void Main(string[] args)
-        {
+        {//Початок виконання програми
 
-        }
+
+        }//Кінець виконання програми 
     }
 }
