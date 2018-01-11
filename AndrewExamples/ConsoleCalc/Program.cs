395010ae9a012cf5376963edb0e3e5cfbcc6aa39
Slawik Iwanenko <good.worker.1997@gmail.com>
2018-01-11 19:44:37 +0200

    Додали циклічне виконання обчислення

--- AndrewExamples/ConsoleCalc/Program.cs
+++ AndrewExamples/ConsoleCalc/Program.cs
@@ -18,51 +18,56 @@ namespace ConsoleCalc
 
             int result;//результат арифметичної операції
 
-            string arithmeticAction; // арифметична дія, яку будемо виконувати
+            string Action; // арифметична дія, яку будемо виконувати
 
-            Console.WriteLine("Введіть арифметичну дію:");
-            arithmeticAction = Console.ReadLine();
-           
-            //ДОДАВАННЯ
-            if (arithmeticAction == "+")
+            while (true)
             {
-                Console.WriteLine("Введіть доданок 1:");
-                left = Convert.ToInt32(Console.ReadLine());//зчитуємо ліву частину
+                Console.WriteLine("Введiть арифметичну дiю або 0 для виходу з програми:");
+                Action = Console.ReadLine();
 
-                Console.WriteLine("Введіть доданок 2");
-                right = Convert.ToInt32(Console.ReadLine());//зчитуємо праву частину
+                //Вихід з програми
+                if (Action == "0")
+                    break;//оператор виходу з циклу
 
-                result = left + right;
-                Console.WriteLine(left + " + " + right + " = " + result);
+                //ДОДАВАННЯ
+                if (Action == "+")
+                {
+                    Console.WriteLine("Введiть доданок 1:");
+                    left = Convert.ToInt32(Console.ReadLine());//зчитуємо ліву частину
+
+                    Console.WriteLine("Введiть доданок 2");
+                    right = Convert.ToInt32(Console.ReadLine());//зчитуємо праву частину
+
+                    result = left + right;
+                    Console.WriteLine(left + " + " + right + " = " + result);
+                }
+
+                //ВІДНІМАННЯ
+                if (Action == "-")
+                {
+                    Console.WriteLine("Введiть зменшуване:");
+                    left = Convert.ToInt32(Console.ReadLine());//зчитуємо ліву частину
+
+                    Console.WriteLine("Введiть вiд'ємник:");
+                    right = Convert.ToInt32(Console.ReadLine());//зчитуємо праву частину
+
+                    result = left - right;
+                    Console.WriteLine(left + " - " + right + " = " + result);
+                }
+
+                //МНОЖЕННЯ
+                if (Action == "*")
+                {
+                    Console.WriteLine("Введiть множник 1:");
+                    left = Convert.ToInt32(Console.ReadLine());//зчитуємо ліву частину
+
+                    Console.WriteLine("Введiть множник 2:");
+                    right = Convert.ToInt32(Console.ReadLine());//зчитуємо праву частину
+
+                    result = left * right;
+                    Console.WriteLine(left + " * " + right + " = " + result);
+                }
             }
-
-            //ВІДНІМАННЯ
-            if (arithmeticAction == "-")
-            {
-                Console.WriteLine("Введіть зменшуване:");
-                left = Convert.ToInt32(Console.ReadLine());//зчитуємо ліву частину
-
-                Console.WriteLine("Введіть від'ємник:");
-                right = Convert.ToInt32(Console.ReadLine());//зчитуємо праву частину
-
-                result = left - right;
-                Console.WriteLine(left + " - " + right + " = " + result);
-            }
-
-            //МНОЖЕННЯ
-            if (arithmeticAction == "*")
-            {
-                Console.WriteLine("Введіть множник 1:");
-                left = Convert.ToInt32(Console.ReadLine());//зчитуємо ліву частину
-
-                Console.WriteLine("Введіть множник 2:");
-                right = Convert.ToInt32(Console.ReadLine());//зчитуємо праву частину
-
-                result = left * right;
-                Console.WriteLine(left + " * " + right + " = " + result);
-            }
-
-            Console.ReadKey();
         }//Кінець виконання програми 
     }
 }
